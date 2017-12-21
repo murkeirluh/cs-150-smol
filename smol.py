77c603b953ab260092e5451b1fdecf284775db48
Alyssa Senatin <alyssasenatin@gmail.com>
2017-12-21 22:50:48 +0800

    Iterative Statements
    changed format to AST for easy interpretation

--- smol.py
+++ smol.py
@@ -131,9 +131,13 @@ else_flag = 1
 
 # start
 # <start> ::= <code-entity>*
-def p_start(p):
-   'start : code_entity'
-   p[0] = p[1]
+def p_start1(p):
+   'start : '
+   pass
+
+def p_start2(p):
+   'start : start code_entity'
+   p[0] = p[2]
    
 # code entity
 # <code-entity> ::= <iterative-statement>
@@ -154,43 +158,28 @@ def p_code_entity(p):
 #                         | for <expression> , <expression> , <expression> : <start> endfor
 def p_iterative_statement_1(p):
    'iterative_statement : WHILE expression COLON start ENDWHILE'
-   while(p[2]):
-     p[4]
+   p[0] = (p.lineno(1), 'while', p[2], p[4])
     
 def p_iterative_statement_2(p):
    'iterative_statement : FOR expression COMMA expression COMMA expression COLON start ENDFOR'
+   p[0] = (p.lineno(1), 'for', p[2], p[4], p[6], p[8])
+
+#def p_for_expression(p):
+   '''for_expression : INTEGER
+                     | IDENTIFIER'''
+#   p[0] = p[1]
     
 # conditional statements
 # <conditional-statement> ::= if <expression> : <start> else <start> endif
 #                           | if <expression> : <start> endif
 def p_conditional_statement_1(p):
-   'conditional_statement : IF expression seen_if COLON start seen_start ELSE seen_else start seen_start ENDIF'
+   'conditional_statement : IF expression COLON start ELSE start ENDIF'
+   p[0] = (p.lineno(1), 'if', p[2], p[4], p[6])
 
 def p_conditional_statement_2(p):
-   'conditional_statement : IF expression seen_if COLON start seen_start ENDIF'
+   'conditional_statement : IF expression COLON start ENDIF'
+   p[0] = (p.lineno(1), 'if', p[2], p[4])
 
-# check if expression is true
-# if false, set flag to 0 (prevents symbol table from updating during then statement)
-def p_seen_if(p):
-   'seen_if : '
-   global then_flag
-   if p[-1] == False:
-     then_flag = 0
-
-# check if expression is true for if-then-else statements
-# if true, set flag to 0 (prevents symbol table from updating during else statement)
-def p_seen_else(p):
-   'seen_else : '
-   global then_flag
-   if p[-5] == True:
-     then_flag = 0
-
-# after then and else statements, set flag to 1
-def p_seen_start(p):
-   'seen_start : '
-   global then_flag
-   then_flag = 1
-      
 # expression
 # <expression> ::= <assignment-statement>
 def p_expression(p):
@@ -206,10 +195,7 @@ def p_assignment_statement_1(p):
     
 def p_assignment_statement_2(p):
     'assignment_statement : IDENTIFIER EQUALS or_statement'
-    if then_flag == 1 and else_flag == 1:
-      if type(p[3]).__name__ != 'NoneType':
-        types[p[1]] = type(p[3]).__name__
-        names[p[1]] = p[3]
+    p[0] = (p.lineno(2), 'assign', p[1], p[3])
 
 # or statement
 # <or-statement> ::= <and-statement>
@@ -220,10 +206,7 @@ def p_or_statement_1(p):
     
 def p_or_statement_2(p):
     'or_statement : or_statement OR and_statement'
-    if (type(p[1]).__name__ != 'list' and type(p[1]).__name__ != 'NoneType') and (type(p[3]).__name__ != 'list' and type(p[3]).__name__ != 'NoneType'):
-      p[0] = p[1] or p[3]
-    else:
-      print("(Runtime) Error at line %d: Incompatible types '%s' and '%s'" % (p.lineno(2), type(p[1]).__name__, type(p[3]).__name__))
+    p[0] = (p.lineno(2), 'or', p[1], p[3])
 
 # and statement
 # <and-statement> ::= <equality-statement>
@@ -234,10 +217,8 @@ def p_and_statement_1(p):
     
 def p_and_statement_2(p):
     'and_statement : and_statement AND equality_statement'
-    if (type(p[1]).__name__ != 'list' and type(p[1]).__name__ != 'NoneType') and (type(p[3]).__name__ != 'list' and type(p[3]).__name__ != 'NoneType'):
-      p[0] = p[1] and p[3]
-    else:
-      print("(Runtime) Error at line %d: Incompatible types '%s' and '%s'" % (p.lineno(2), type(p[1]).__name__, type(p[3]).__name__))
+    
+    p[0] = (p.lineno(2), 'and', p[1], p[3])
     
 # equality statement
 # <equality-statement> ::= <relational-statement>
@@ -250,13 +231,7 @@ def p_equality_statement_1(p):
 def p_equality_statement_2(p):
     '''equality_statement : equality_statement EQ relational_statement
                           | equality_statement NEQ relational_statement'''
-    if (type(p[1]).__name__ != 'list' and type(p[1]).__name__ != 'NoneType') and (type(p[3]).__name__ != 'list' and type(p[3]).__name__ != 'NoneType'):
-      if p[2] == '==':
-        p[0] = (p[1] == p[3])
-      elif p[2] == '!=':
-        p[0] = not (p[1] == p[3])
-    else:
-      print("(Runtime) Error at line %d: Incompatible types '%s' and '%s'" % (p.lineno(2), type(p[1]).__name__, type(p[3]).__name__))
+    p[0] = (p.lineno(2), 'equality', p[2], p[1], p[3])
     
 # relational statement
 # <relational-statement> ::= <add-statement>
@@ -273,17 +248,7 @@ def p_relational_statement_2(p):
                             | relational_statement GT add_statement
                             | relational_statement LTEQ add_statement
                             | relational_statement GTEQ add_statement'''
-    if (type(p[1]).__name__ != 'list' and type(p[1]).__name__ != 'NoneType') and (type(p[3]).__name__ != 'list' and type(p[3]).__name__ != 'NoneType'):
-      if p[2] == '<':
-        p[0] = (p[1] < p[3])
-      elif p[2] == '>':
-        p[0] = (p[1] > p[3])
-      elif p[2] == '<=':
-        p[0] = (p[1] <= p[3])
-      elif p[2] == '>=':
-        p[0] = (p[1] >= p[3])
-    else:
-      print("(Runtime) Error at line %d: Incompatible types '%s' and '%s'" % (p.lineno(2), type(p[1]).__name__, type(p[3]).__name__))
+    p[0] = (p.lineno(2), 'relational', p[2], p[1], p[3])
 
 # add statement
 # <add-statement> ::= <multiply-statement>
@@ -296,13 +261,7 @@ def p_add_statement_1(p):
 def p_add_statement_2(p):
     '''add_statement : add_statement PLUS multiply_statement
                      | add_statement MINUS multiply_statement'''
-    if (type(p[1]).__name__ != 'list' and type(p[1]).__name__ != 'NoneType') and (type(p[3]).__name__ != 'list' and type(p[3]).__name__ != 'NoneType'):
-      if p[2] == '+':
-        p[0] = p[1] + p[3]
-      elif p[2] == '-':
-        p[0] = p[1] - p[3]
-    else:
-      print("(Runtime) Error at line %d: Incompatible types '%s' and '%s'" % (p.lineno(2), type(p[1]).__name__, type(p[3]).__name__))
+    p[0] = (p.lineno(2), 'add', p[2], p[1], p[3])
 
 # multiply statement
 # <multiply-statement> ::= <unary-statement>
@@ -317,15 +276,7 @@ def p_multiply_statement_2(p):
     '''multiply_statement : multiply_statement TIMES unary_statement
                           | multiply_statement DIVIDE unary_statement
                           | multiply_statement MOD unary_statement'''
-    if (type(p[1]).__name__ != 'list' and type(p[1]).__name__ != 'NoneType') and (type(p[3]).__name__ != 'list' and type(p[3]).__name__ != 'NoneType'):
-      if p[2] == '*':
-        p[0] = p[1] * p[3]
-      elif p[2] == '/':
-        p[0] = p[1] / p[3]
-      elif p[2] == '%':
-        p[0] = p[1] % p[3]
-    else:
-      print("(Runtime) Error at line %d: Incompatible types '%s' and '%s'" % (p.lineno(2), type(p[1]).__name__, type(p[3]).__name__))
+    p[0] = (p.lineno(2), 'multiply', p[2], p[1], p[3])
  
 # unary statement
 # <unary-statement> ::= <exponent>
@@ -338,10 +289,7 @@ def p_unary_statement_1(p):
 def p_unary_statement_2(p):
     '''unary_statement : MINUS unary_statement
                        | NOT unary_statement'''
-    if p[1] == '-':
-      p[0] = -p[2]
-    elif p[1] == '!':
-      p[0] = not p[2]
+    p[0] = (p.lineno(1), 'unary', p[1], p[2])
 
 # exponent
 # <exponent> ::= <term>
@@ -352,7 +300,7 @@ def p_exponent_1(p):
     
 def p_exponent_2(p):
     'exponent : term POWER unary_statement'
-    p[0] = p[1] ** p[3]
+    p[0] = (p.lineno(2), 'exponent', p[1], p[3])
 
 # term
 # <term> ::= IDENTIFIER
@@ -360,10 +308,7 @@ def p_exponent_2(p):
 #          | ( <expression> )    
 def p_term_1(p):
     'term : IDENTIFIER'
-    if p[1] in types:
-        p[0] = names[p[1]]
-    else:
-      print("(Runtime) Error at line %d: '%s' not defined" % (p.lineno(1), p[1]))
+    p[0] = (p.lineno(1), 'identifier', p[1])
     
 def p_term_2(p):
     'term : atom'
@@ -383,59 +328,44 @@ def p_term_3(p):
 #          | IDENTIFIER [ INTEGER ]   
 def p_atom1(p):
    'atom : INTEGER'
-   p[0] = p[1]
+   p[0] = (p.lineno(1), 'atom', p[1])
 
 def p_atom2(p):
    'atom : FLOAT'
-   p[0] = p[1]
+   p[0] = (p.lineno(1), 'atom', p[1])
 
 def p_atom3(p):
    'atom : CHARACTER'
-   p[0] = p[1]
+   p[0] = (p.lineno(1), 'atom', p[1])
 
 def p_atom4(p):
    'atom : STRING'
-   p[0] = p[1]
+   p[0] = (p.lineno(1), 'atom', p[1])
 
 def p_atom5(p):
    'atom : TRUE'
-   p[0] = True
+   p[0] = (p.lineno(1), 'atom', p[1])
 
 def p_atom6(p):
    'atom : FALSE'
-   p[0] = False
-
-global array
-array = []
+   p[0] = (p.lineno(1), 'atom', p[1])
 
 def p_atom7(p):
    'atom : LBRACKET elements RBRACKET'
-   global array
-   array = []
-   p[0] = p[2]
+   p[0] = (p.lineno(1), 'atom-array', p[2])
 
 def p_atom8(p):
    'atom : IDENTIFIER LBRACKET INTEGER RBRACKET'
-   getarray = []
-   if p[1] in names:
-     getarray = names[p[1]]
-     if p[3] < len(getarray):
-       p[0] = getarray[p[3]]
-     else:
-       print("(Runtime) Error at line %d: Index is out of range" % p.lineno(3))
-   else:
-     print("(Runtime) Error at line %d: '%s' not declared" % (p.lineno(1), p[1]))
+   p[0] = (p.lineno(1), 'atom-indexing', p[1], p[3])
 
 # elements in array
 def p_elements1(p):
     'elements : elements INTEGER'
-    array.append(p[2])
-    p[0] = array
+    p[0] = (p[1], p[2])
 
 def p_elements2(p):
     'elements : elements FLOAT'
-    array.append(p[2])
-    p[0] = array
+    p[0] = (p[1], p[2])
 
 def p_elements3(p):
     'elements : '
@@ -445,29 +375,166 @@ def p_elements3(p):
 # <input-function> ::= input( IDENTIFIER )    
 def p_input_function(p):
     'input_function : INPUT LPAREN IDENTIFIER RPAREN' # input(variable)
-    inp = input()
-    if p[3] in types:
-      inp = parser.parse(inp)
-      if type(inp).__name__ == types[p[3]]:
-        if p[3] in names:
-          del names[p[3]]
-
-        names[p[3]] = inp
-      else:
-        print("(Runtime) Error at line %d: '%s' is not of type '%s'" % (p.lineno(3), inp, types[p[3]]));
-    else:
-      print("(Runtime) Error at line %d: '%s' not declared" % (p.lineno(3), p[3]))    
+    p[0] = (p.lineno(1), 'input', p[3])  
 
 # output function
 # <output-function> ::= print( (“<atom>” | IDENTIFIER) {+ (“<atom>” | IDENTIFIER)+} )
 def p_output_function(p):
     'output_function : PRINT LPAREN term RPAREN'
-    print(p[3])
+    p[0] = (p.lineno(1), 'output', p[3])
 
 # error in syntax
 def p_error(p):
     if p:
-      print("(Syntax) Error at %s" % p.value)    
+      print("(Syntax) Error at %s" % p.value)
+    else:
+      print("(Syntax) Error at EOF")
+
+def interpreter(result):
+    if result[1] == 'while':
+      while (interpreter(result[2])):
+        interpreter(result[3])
+
+    if result[1] == 'for':
+      interpreter(result[2])
+      while (interpreter(result[3])):
+        interpreter(result[5])
+        interpreter(result[4])
+
+    if result[1] == 'if':
+      value = interpreter(result[2])
+      if value == True:
+        interpreter(result[3])
+      else:
+        if len(result) == 5:
+          interpreter(result[4])
+
+    if result[1] == 'assign':
+      var = result[2]
+      value = interpreter(result[3])
+      names[var] = value
+      types[var] = type(value).__name__
+
+    if result[1] == 'or':
+      value1 = interpreter(result[2])
+      value2 = intepreter(result[3])
+      if type(value1).__name__ != 'list' and type(value1).__name__ != 'NoneType' and type(value2).__name__ != 'list' and type(value2).__name__ != 'NoneType':
+        return value1 or value2
+      else:
+        print("(Runtime) Error at line %d: Incompatible types '%s' and '%s'" % (result[0], type(value1).__name__, type(value2).__name__))
+
+    if result[1] == 'and':
+      value1 = interpreter(result[2])
+      value2 = intepreter(result[3])
+      if type(value1).__name__ != 'list' and type(value1).__name__ != 'NoneType' and type(value2).__name__ != 'list' and type(value2).__name__ != 'NoneType':
+        return value1 and value2
+      else:
+        print("(Runtime) Error at line %d: Incompatible types '%s' and '%s'" % (result[0], type(value1).__name__, type(value2).__name__))
+
+    if result[1] == 'equality':
+      value1 = interpreter(result[3])
+      value2 = interpreter(result[4])
+      if type(value1).__name__ != 'list' and type(value1).__name__ != 'NoneType' and type(value2).__name__ != 'list' and type(value2).__name__ != 'NoneType':
+        if result[2] == '==':
+          return value1 == value2
+        if result[2] == '!=':
+          return value1 != value2
+      else:
+        print("(Runtime) Error at line %d: Incompatible types '%s' and '%s'" % (result[0], type(value1).__name__, type(value2).__name__))
+
+    if result[1] == 'relational':
+      value1 = interpreter(result[3])
+      value2 = interpreter(result[4])
+      if type(value1).__name__ != 'list' and type(value1).__name__ != 'NoneType' and type(value2).__name__ != 'list' and type(value2).__name__ != 'NoneType':
+        if result[2] == '<':
+          return value1 < value2
+        if result[2] == '>':
+          return value1 > value2
+        if result[2] == '<=':
+          return value1 <= value2
+        if result[2] == '>=':
+          return value1 >= value2
+      else:
+        print("(Runtime) Error at line %d: Incompatible types '%s' and '%s'" % (result[0], type(value1).__name__, type(value2).__name__))
+
+    if result[1] == 'add':
+      value1 = interpreter(result[3])
+      value2 = interpreter(result[4])
+      if type(value1).__name__ != 'int' and type(value1).__name__ != 'float' and type(value2).__name__ != 'int' and type(value2).__name__ != 'float':
+        if result[2] == '+':
+          return value1 + value2
+        if result[2] == '-':
+          return value1 - value2
+      else:
+        print("(Runtime) Error at line %d: Incompatible types '%s' and '%s'" % (result[0], type(value1).__name__, type(value2).__name__))
+
+    if result[1] == 'multiply':
+      value1 = interpreter(result[3])
+      value2 = interpreter(result[4])
+      if type(value1).__name__ != 'int' and type(value1).__name__ != 'float' and type(value2).__name__ != 'int' and type(value2).__name__ != 'float':
+        if result[2] == '*':
+          return value1 * value2
+        if result[2] == '/':
+          return value1 / value2
+        if result[2] == '%':
+          return value1 % value2
+      else:
+        print("(Runtime) Error at line %d: Incompatible types '%s' and '%s'" % (result[0], type(value1).__name__, type(value2).__name__))
+
+    if result[1] == 'unary':
+      value = interpreter(result[3])
+      if result[2] == '-':
+        return -result[4]
+      if result[2] == '!':
+        return not result[4]
+
+    if result[1] == 'exponent':
+      value1 = interpreter(result[3])
+      value2 = interpreter(result[4])
+      return value1 ** value2
+
+    if result[1] == 'identifier':
+      if result[2] in names:
+        return names[result[2]]
+      else:
+        print("(Runtime) Error at line %d: '%s' is not defined" % (result[0], result[2]))
+
+    if result[1] == 'atom':
+      return result[2]
+
+    def addelements(tuple):
+      array.insert(0, tuple[1])
+      if tuple[0] != None:
+        addelements(tuple[0])
+
+    if result[1] == 'atom-array':
+      array = []
+      addelements(result[2])
+      return array
+
+    if result[1] == 'atom-indexing':
+      if result[2] in names:
+        array = names[result[2]]
+        return array[result[3]]
+      else:
+        print("(Runtime) Error at line %d: '%s' is not defined" % (result[0], result[2]))
+
+    if result[1] == 'input':
+      if result[2] in types:
+        inp = input()
+        inp = interpreter(parser.parse(inp))
+        if type(inp).__name__ == types[result[2]]:
+          if result[2] in names:
+            del names[result[2]]
+          names[result[2]] = inp
+        else:
+          print("(Runtime) Error at line %d: '%s' is not of type '%s'" % (result[0], inp, types[result[2]]))
+      else:
+        print("(Runtime) Error at line %d: '%s' not declared" % (result[0], result[2]))
+
+    if result[1] == 'output':
+      value = interpreter(result[2])
+      print(value)
 
 import ply.yacc as yacc
 parser = yacc.yacc()
@@ -479,5 +546,8 @@ while True:
         break
     if not s: continue
     result = parser.parse(s) #, tracking=True)
-    if type(result).__name__ != 'NoneType':
-      print(result)
+    if result != None:
+      interpreter(result)
+      if result[1] == 'identifier':
+        if result[2] in names:
+          print(names[result[2]])
